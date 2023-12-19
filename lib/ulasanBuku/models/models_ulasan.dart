572c5a58bb885defb6158e6c3899fc4dcb4b698e
eryanda <eryanduty@gmail.com>
2023-12-19 11:15:17 +0700

    almost done

--- lib/ulasanBuku/models/models_ulasan.dart
+++ lib/ulasanBuku/models/models_ulasan.dart
@@ -1,17 +1,65 @@
-// ulasanBuku/models/model_ulasan.dart
-class BookReview {
-  final String username;
-  String nama;
-  String tanggalReview;
-  String reviewText;
-  String rating;
+// To parse this JSON data, do
+//
+//     final review = reviewFromJson(jsonString);
 
-  BookReview({
-    required this.username,
-    required this.nama,
-    required this.tanggalReview,
-    required this.reviewText,
-    required this.rating,
-    // Tambahkan bidang lain sesuai kebutuhan
-  });
+import 'dart:convert';
+
+List<Review> reviewFromJson(String str) => List<Review>.from(json.decode(str).map((x) => Review.fromJson(x)));
+
+String reviewToJson(List<Review> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Review {
+    String model;
+    int pk;
+    Fields fields;
+
+    Review({
+        required this.model,
+        required this.pk,
+        required this.fields,
+    });
+
+    factory Review.fromJson(Map<String, dynamic> json) => Review(
+        model: json["model"],
+        pk: json["pk"],
+        fields: Fields.fromJson(json["fields"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "model": model,
+        "pk": pk,
+        "fields": fields.toJson(),
+    };
+}
+
+class Fields {
+    int user;
+    int book;
+    int rating;
+    String content;
+    DateTime createdAt;
+
+    Fields({
+        required this.user,
+        required this.book,
+        required this.rating,
+        required this.content,
+        required this.createdAt,
+    });
+
+    factory Fields.fromJson(Map<String, dynamic> json) => Fields(
+        user: json["user"],
+        book: json["book"],
+        rating: json["rating"],
+        content: json["content"],
+        createdAt: DateTime.parse(json["created_at"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "user": user,
+        "book": book,
+        "rating": rating,
+        "content": content,
+        "created_at": createdAt.toIso8601String(),
+    };
 }
