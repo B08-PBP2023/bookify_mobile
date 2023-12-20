2008d8f02cb753df055544d2f04344437a372c42
alifbintang1 <alifbintange@gmail.com>
2023-12-20 18:49:37 +0700

    edit dikit

--- lib/homepage/drawer.dart
+++ lib/homepage/drawer.dart
@@ -1,5 +1,5 @@
-// import 'package:bookify_mobile/ulasanBuku/screen/list_Ulasan.dart';
 import 'package:bookify_mobile/faq/screen/list_buku_faq.dart';
+import 'package:bookify_mobile/ulasanBuku/screen/list_Ulasan.dart';
 import 'package:flutter/material.dart';
 import 'package:bookify_mobile/book_page/screen/list_buku.dart';
 import 'package:bookify_mobile/homepage/homepage.dart';
@@ -109,16 +109,16 @@ Drawer buildDrawer(BuildContext context) {
                     );
                   },
                 ),
-                // ListTile(
-                //   title: const Text('Ulasan', style: TextStyle(color: Colors.white)),
-                //   leading: const Icon(Icons.reviews, color: Colors.white),
-                //   onTap: () {
-                //     Navigator.pushReplacement(
-                //       context,
-                //       MaterialPageRoute(builder: (context) => const DaftarUlasan()),
-                //     );
-                //   },
-                // ),
+                ListTile(
+                  title: const Text('Ulasan', style: TextStyle(color: Colors.white)),
+                  leading: const Icon(Icons.reviews, color: Colors.white),
+                  onTap: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const DaftarUlasan()),
+                    );
+                  },
+                ),
 
               ],
             ),
