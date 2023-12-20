dee36048a6934cf0b52c6c96a3cff3dc7a51ffbc
eryanda <eryanduty@gmail.com>
2023-12-20 16:40:55 +0700

    ulasanBuku done

--- lib/ulasanBuku/screen/ulasan_page.dart
+++ lib/ulasanBuku/screen/ulasan_page.dart
@@ -1,128 +1,192 @@
 import 'dart:convert';
-
-import 'package:bookify_mobile/faq/screen/question_with_answer.dart';
-import 'package:flutter/material.dart';
-import 'package:pbp_django_auth/pbp_django_auth.dart';
-import 'package:provider/provider.dart';
-// import 'package:shopping_list/screens/menu.dart';
-// // TODO: Impor drawer yang sudah dibuat sebelumnya
-// import 'package:shopping_list/widgets/left_drawer.dart';
-import 'dart:convert';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:bookify_mobile/ulasanBuku/models/models_ulasan.dart';
+import 'package:bookify_mobile/ulasanBuku/utils/fetch_ulasan.dart';
+import 'package:bookify_mobile/profilUser/profile_page.dart';
 import 'package:http/http.dart' as http;
 
 class UlasanPage extends StatefulWidget {
-    const UlasanPage({super.key, required this.idBuku});
-    final int idBuku;
-    @override
-    State<UlasanPage> createState() => _UlasanPageState();
+  const UlasanPage({super.key, required this.idBuku});
+  final int idBuku;
+
+  @override
+  _UlasanPageState createState() => _UlasanPageState();
 }
 
 class _UlasanPageState extends State<UlasanPage> {
-    final _formKey = GlobalKey<FormState>();
-    String _isi_ulasan = "";
-    
-    @override
-    Widget build(BuildContext context) {
-      int idBuku = widget.idBuku;
-      final request = context.watch<CookieRequest>();
-      return Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Text(
-              'Form Tambahkan Ulasan',
-            ),
-          ),
-          backgroundColor: Colors.indigo,
-          foregroundColor: Colors.white,
-        ),
-        // TODO: Tambahkan drawer yang sudah dibuat di sini
-        body: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Pertanyaan",
-                      labelText: "Pertanyaan",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0),
-                      ),
-                    ),
-                    onChanged: (String? value) {
-                      setState(() {
-                        _isi_ulasan = value!;
-                      });
-                    },
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return "Pertanyaan tidak boleh kosong!";
-                      }
-                      return null;
-                    },
-                  ),
-                ),
+  TextEditingController inputController = TextEditingController();
+  String hasilUlasan = "";
+  String ratingTerpilih = '1';
+  String formatDate(DateTime date) {
+    final DateFormat formatter = DateFormat('dd MMMM yyyy');
+    return formatter.format(date);
+  }
 
-            
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.indigo),
-                      ),
-                      onPressed: () async {
-                          if (_formKey.currentState!.validate()) {
-                              // Kirim ke Django dan tunggu respons
-                              // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
-                              final response = await request.postJson(
-                              "https://bookify-b08-tk.pbp.cs.ui.ac.id/ulasanBuku/add_ulasan_flutter/$idBuku/",
-                              // "http://127.0.0.1:8000/FAQ/add_question_flutter/$idBuku/",
+  void kirimUlasanKeServer() async {
+    // Ganti URL sesuai dengan endpoint API server Anda
+    final url =
+        "https://bookify-b08-tk.pbp.cs.ui.ac.id/ulasanBuku/add_ulasan_flutter/${widget.idBuku}/";
+    // "http://127.0.0.1:8000/ulasanBuku/add_ulasan_flutter/${widget.idBuku}/";
 
-                              jsonEncode(<String, String>{
-                                  'isi_ulasan': _isi_ulasan,
-                                  // TODO: Sesuaikan field data sesuai dengan aplikasimu
-                              }));
-                              if (response['status'] == 'success') {
-                                  ScaffoldMessenger.of(context)
-                                      .showSnackBar(const SnackBar(
-                                  content: Text("Pertanyaan baru berhasil disimpan!"),
-                                  ));
-                                  Navigator.pushReplacement(
-                                      context,
-                                      MaterialPageRoute(builder: (context) => FAQBuku(idBuku: idBuku,)),
-                                  );
-                              } else {
-                                  ScaffoldMessenger.of(context)
-                                      .showSnackBar(const SnackBar(
-                                      content:
-                                          Text("Terdapat kesalahan, silakan coba lagi."),
-                                  ));
-                              }
-                          }
-
-                      },
-                      child: const Text(
-                        "Save",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-
-              ],
-            )
-          ),
-        ),
+    try {
+      final request = context.read<CookieRequest>();
+      final response = await request.postJson(
+        url,
+        jsonEncode(<String, dynamic>{
+          'rating': ratingTerpilih,
+          'isi_ulasan': hasilUlasan,
+        }),
+      );
+      // print(hasilUlasan);
+      // print(ratingTerpilih);
+      if (response['status'] == 'success') {
+        // ScaffoldMessenger.of(context).showSnackBar(
+        //   SnackBar(content: Text("Ulasan berhasil dikirim!")),
+        // );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Gagal mengirim ulasan, coba lagi.")),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Terjadi kesalahan: $e")),
       );
     }
+  }
+
+  void tambahUlasan() async {
+    hasilUlasan = inputController.text;
+    kirimUlasanKeServer();
+    setState(() {
+      // Bersihkan input setelah submit
+      inputController.clear();
+      ratingTerpilih = '1';
+    });
+  }
+
+  // Widget untuk menampilkan satu ulasan
+  Widget buildReviewCard(Review ulasan) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: ListTile(
+        title: Text(
+          ulasan.fields.content,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Rating: ${ulasan.fields.rating}"),
+            Text("Tanggal dibuat: ${formatDate(ulasan.fields.createdAt)}"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ulasan Buku"),
+        backgroundColor: Color.fromARGB(255, 91, 84, 230),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Card(
+                color: Colors.lightBlue[50],
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Kotak Ulasan",
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      TextFormField(
+                        controller: inputController,
+                        maxLines: 5,
+                        decoration: InputDecoration(
+                          hintText: "Tulis ulasan Anda di sini",
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      DropdownButtonFormField<String>(
+                        value: ratingTerpilih,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            ratingTerpilih = newValue!;
+                          });
+                        },
+                        items: <String>['1', '2', '3', '4', '5']
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        decoration: InputDecoration(
+                          labelText: 'Rating',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      ElevatedButton(
+                        child: Text("Submit Ulasan"),
+                        onPressed: tambahUlasan,
+                        style: ElevatedButton.styleFrom(primary: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              // Tampilkan daftar ulasan
+              Text(
+                "Daftar Ulasan",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                ),
+              ),
+              FutureBuilder<List<Review>>(
+                future: fetchUlasan(widget.idBuku),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text("Error: ${snapshot.error}"));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Center(child: Text("Tidak ada ulasan"));
+                  } else {
+                    return ListView(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      children: snapshot.data!.map(buildReviewCard).toList(),
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
